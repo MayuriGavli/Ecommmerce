c6e088b6fccaa08de73919b6e522cfdb0b7fb385
GAVLI MAYURI <mrgavli9@gmail.com>
2024-01-12 22:19:08 +0530

    Update the code

--- lib/database.dart
+++ lib/database.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
 
 class DBHelper {
-  static Database _database;
+  late Database _database;
 
   Future<Database> get database async {
     if (_database != null) return _database;
@@ -15,19 +13,18 @@ class DBHelper {
   }
 
   Future<Database> initDatabase() async {
-    Directory documentsDirectory = await getApplicationDocumentsDirectory();
-    String path = join(documentsDirectory.path, 'your_database_name.db');
-    return await openDatabase(path, version: 1, onCreate: _createTables);
-  }
-
-  void _createTables(Database db, int version) async {
-    // Create your tables here
-    await db.execute('''
-      CREATE TABLE your_table_name (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        age INTEGER
-      )
-    ''');
+    return openDatabase(
+      join(await getDatabasesPath(), 'your_database_name.db'),
+      onCreate: (db, version) {
+        return db.execute('''
+          CREATE TABLE your_table_name (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            age INTEGER
+          )
+        ''');
+      },
+      version: 1,
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@
 // import 'dart:ui';
 
 import 'package:flutter/material.dart';
+
 import 'homePage.dart';
-import 'loginPage.dart';
 
 void main() {
   runApp(const MaterialApp(
